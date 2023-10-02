47fd61966ae57b334d0dcb215b413424a63b4a1d
Tsotne Darjania <tsotnedarjania1997@gmail.com>
2023-10-02 18:03:40 +0400

    add loading component

--- app/components/loading/Loading.tsx
+++ app/components/loading/Loading.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import style from "./style.module.css";
 
 const Loading = () => {
   return (
-    <div>Loading</div>
-  )
-}
+    <div className={style.loading}>
+      <div className={style.content}>
+        <p>Loading </p>
+        <p className={style.pointAnime1}>.</p>
+        <p className={style.pointAnime2}>.</p>
+        <p className={style.pointAnime3}>.</p>
+      </div>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
